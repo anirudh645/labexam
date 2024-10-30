1bec1914378635e6f9a42ab6880142db4d77686d
CHUNDURU KESAVA MOHAN GANESH <22bq1a1233@vvit.net>
2024-10-30 07:35:02 +0530

    Update FParentContext.js

--- FParentContext.js
+++ FParentContext.js
@@ -2,7 +2,7 @@ import React,{useState,createContext} from "react";
 import FChildContext from "./FChildContext";
 export const Context = createContext();
 function FParentContext(){
-    const [name, ChangeName] = useState("Vamsi");
+    const [name, ChangeName] = useState("Vvit");
     return (
         <Context.Provider value = {name}>
             <h1>Parent - {name}</h1>
